f235764351b7b5aa336ea376955c3311f7289ba7
dev-andressantana <7a3e9s1h5@duck.com>
2023-06-22 05:37:05 -0400

    Development Log
    fixed node 'data' request; data is not dict but was called as one.

--- src/physics_nodes.py
+++ src/physics_nodes.py
@@ -8,7 +8,7 @@ class Body(fuel.Node):
 
     def __init__(self, name, data):
         super().__init__(name)
-        self.image = pygame.image.load(data['image'])
+        self.image = pygame.image.load(data[0])
         self.speed = [0,0]
         self.collider = self.image.get_rect()
         self.motion = self.collider.move(self.speed)
